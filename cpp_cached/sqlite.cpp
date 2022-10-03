3a520ae363c29c5e2ef1a22ed69352092bc83807
reder2000 <rouge.richard@free.fr>
2022-10-04 00:20:48 +0200

    add compiler + debug in sql file name

--- cpp_cached/sqlite.cpp
+++ cpp_cached/sqlite.cpp
@@ -1,14 +1,32 @@
 #include "sqlite.h"
 
-
 using namespace  SQLite;
+
+std::string machine_name()
+{
+#if _MSC_VER
+  #if _DEBUG
+  return "MSC_DEBUG_";
+  #else
+  return "MSC_RELEASE_";
+  #endif
+#else
+#if _DEBUG
+  return "XXX_DEBUG_";
+#else
+  return "XXX_RELEASE_";
+#endif
+#endif
+
+}
 
 SqliteCache::SqliteCache(const std::filesystem::path& root_path, uint64_t max_size) :
 	_root_path(root_path), _max_size(max_size)
 {
-	_db_name = _root_path / "cache.db";
-	if (!std::filesystem::exists(_root_path))
-		MREQUIRE(std::filesystem::create_directories(_root_path));
+  auto rerroot = _root_path / machine_name();
+	_db_name =  rerroot / "cache.db";
+  if (! std::filesystem::exists(rerroot))
+          MREQUIRE(std::filesystem::create_directories(rerroot));
 	auto db = get_db(true);
 }
 
