4772d2182495df345b19c47cf9ca22916ae5cfb7
reder2000 <rouge.richard@free.fr>
2023-01-24 03:21:16 +0100

    protect sqlite from python multiprocessing

--- cpp_cached/sqlite.cpp
+++ cpp_cached/sqlite.cpp
@@ -1,15 +1,19 @@
 #include "sqlite.h"
+#include "sqlite3.h"
 
-using namespace  SQLite;
+using namespace SQLite;
+
+constexpr int NB_RETRIES = 50;
+constexpr int BUSYTIME   = 2000;
 
 std::string machine_name()
 {
 #if _MSC_VER
-  #if _DEBUG
+#if _DEBUG
   return "MSC_DEBUG_";
-  #else
+#else
   return "MSC_RELEASE_";
-  #endif
+#endif
 #else
 #if _DEBUG
   return "XXX_DEBUG_";
@@ -17,110 +21,249 @@ std::string machine_name()
   return "XXX_RELEASE_";
 #endif
 #endif
-
 }
 
-SqliteCache::SqliteCache(const std::filesystem::path& root_path, uint64_t max_size) :
-	_root_path(root_path), _max_size(max_size)
+SqliteCache::SqliteCache(const std::filesystem::path& root_path, uint64_t max_size)
+    : _root_path(root_path), _max_size(max_size)
 {
   auto rerroot = _root_path / machine_name();
-	_db_name =  rerroot / "cache.db";
-  if (! std::filesystem::exists(rerroot))
-          MREQUIRE(std::filesystem::create_directories(rerroot));
-	auto db = get_db(true);
+  _db_name     = rerroot / "cache.db";
+  if (! std::filesystem::exists(rerroot)) MREQUIRE(std::filesystem::create_directories(rerroot));
+  create_db();
 }
 
-Database SqliteCache::get_db(bool create_tables)
+void SqliteCache::create_db()
 {
+  try
+  {
+    Database db(_db_name.string(), OPEN_READWRITE | OPEN_CREATE);
+    //db.setBusyTimeout(1000);
+    exec_retry(db,
+               "CREATE TABLE IF NOT EXISTS Cache ("
+               " key TEXT PRIMARY KEY,"
+               " store_time INTEGER,"
+               " expire_time INTEGER,"
+               " access_time INTEGER,"
+               " access_count INTEGER DEFAULT 0,"
+               " size INTEGER DEFAULT 0,"
+               " value BLOB)");
 
-	Database db(_db_name.string(), OPEN_READWRITE | OPEN_CREATE);
+    exec_retry(db,
+               "CREATE INDEX IF NOT EXISTS Cache_expire_time ON"
+               " Cache (expire_time)");
+    exec_retry(db,
+               "CREATE INDEX IF NOT EXISTS Cache_access_time ON"
+               " Cache (access_time)");
+    exec_retry(db,
+               "CREATE INDEX IF NOT EXISTS Cache_access_count ON"
+               " Cache (access_count)");
+  }
+  catch (std::exception& e)
+  {
+    throw std::runtime_error(fmt::format("create_db failed {}\n", e.what()));
+  }
+}
 
-	if (create_tables) {
-
-		db.exec(
-			"CREATE TABLE IF NOT EXISTS Cache ("
-			" key TEXT PRIMARY KEY,"
-			" store_time INTEGER,"
-			" expire_time INTEGER,"
-			" access_time INTEGER,"
-			" access_count INTEGER DEFAULT 0,"
-			" size INTEGER DEFAULT 0,"
-			" value BLOB)");
-
-		db.exec(
-			"CREATE INDEX IF NOT EXISTS Cache_expire_time ON"
-			" Cache (expire_time)"
-		);
-		db.exec(
-			"CREATE INDEX IF NOT EXISTS Cache_access_time ON"
-			" Cache (access_time)"
-		);
-		db.exec(
-			"CREATE INDEX IF NOT EXISTS Cache_access_count ON"
-			" Cache (access_count)"
-		);
-
-
-	}
-
-	return db;
+Database SqliteCache::get_db(bool can_write)
+{
+  try
+  {
+    if (can_write)
+    {
+      Database db(_db_name.string(), OPEN_READWRITE);
+      return db;
+    }
+    Database db(_db_name.string(), OPEN_READONLY);
+    return db;
+  }
+  catch (std::exception& e)
+  {
+    throw std::runtime_error(fmt::format("get_db failed {}\n", e.what()));
+  }
 }
 
 bool SqliteCache::has(const std::string& key)
 {
-	auto db = get_db();
-	Statement query(db, "SELECT key FROM cache WHERE key = ?");
-	query.bind(1, key);
-	return query.executeStep() != 0 ;
-
+  std::string where = "has:get";
+  try
+  {
+    auto db         = get_db(false);
+    where           = "has:query";
+    Statement query = query_retry(db, "SELECT key FROM cache WHERE key = ?");
+    where           = "has:bind";
+    query.bind(1, key);
+    where = "has::executeStep_retry";
+    return executeStep_retry(db, query) != 0;
+  }
+  catch (const std::exception& e)
+  {
+    throw std::runtime_error(fmt::format("has failed : {} : {}", where, e.what()));
+  }
 }
 
 void SqliteCache::erase(const std::string& key)
 {
-	if (!has(key)) return;
-	auto db = get_db();
-	Statement query(db, "DELETE FROM cache where key=?");
-	query.bind(1, key);
-	query.exec();
+  if (! has(key)) return;
+  auto      db = get_db(true);
+  Statement query(db, "DELETE FROM cache where key=?");
+  query.bind(1, key);
+  query.exec();
 }
 
 size_t SqliteCache::total_size(Database& db) const
 {
-	Statement   query(db, "SELECT SUM(size) FROM cache");
-	query.executeStep();
-	size_t res = query.getColumn(0).getInt();
-	return res;
+  Statement query(db, "SELECT SUM(size) FROM cache");
+  query.executeStep();
+  size_t res = query.getColumn(0).getInt();
+  return res;
 }
 
 void SqliteCache::clean_expired(Database& db)
 {
-	int64_t cnow = std__chrono::utc_clock::now().time_since_epoch().count();
+  int64_t cnow = std__chrono::utc_clock::now().time_since_epoch().count();
 
-	Statement query(db, "DELETE FROM cache WHERE expire_time < ?");
-	query.bind(1, cnow);
-	query.exec();
-	//std::vector<std::string> keys;
-	//while (query.executeStep())
-	//{
-	//	keys.push_back(query.getColumn(0).getString());
-	//}
-	//query.reset();
+  Statement query(db, "DELETE FROM cache WHERE expire_time < ?");
+  query.bind(1, cnow);
+  query.exec();
+  //std::vector<std::string> keys;
+  //while (query.executeStep())
+  //{
+  //	keys.push_back(query.getColumn(0).getString());
+  //}
+  //query.reset();
 }
 
 void SqliteCache::clean_older(Database& db, int64_t need_to_free)
 {
+  Statement query(db, "SELECT key,sIzE FROM cache ORDER BY store_time");
+  int64_t   fred = 0;
+  while (query.executeStep() && fred < need_to_free)
+  {
+    auto key = query.getColumn(0).getString();
+    auto sz  = query.getColumn(1).getInt64();
+    fred += sz;
+    Statement query2(db, "DELETE FROM cache where key=?");
+    query2.bind(1, key);
+    query2.exec();
+  }
+}
 
-	Statement query(db, "SELECT key,sIzE FROM cache ORDER BY store_time");
-	int64_t fred = 0;
-	while (query.executeStep() && fred < need_to_free)
-	{
-		auto key = query.getColumn(0).getString();
-		auto sz = query.getColumn(1).getInt64();
-		fred += sz;
-		Statement query2(db, "DELETE FROM cache where key=?");
-		query2.bind(1, key);
-		query2.exec();
-	}
+#define SQLITE_OK 0 /* Successful result */
+//extern "C" int sqlite3_changes(sqlite3*);
+
+int try_exec_retry(SQLite::Database& db, const char* apQueries, int nb_retries)
+{
+  int res;
+  while (nb_retries--)
+  {
+    res = db.tryExec(apQueries);
+    if (res == SQLITE_OK)
+    {
+      return sqlite3_changes(db.getHandle());
+    }
+    else
+    {
+      std::this_thread::sleep_for(std::chrono::milliseconds(BUSYTIME));
+    }
+  }
+  //    if (SQLITE_OK != res && SQLITE_DONE != res && SQLITE_BUSY != res && SQLITE_LOCKED != res)
+
+  throw std::runtime_error(
+      fmt::format("exec, giving up : {}", SQLite::Exception(db.getHandle(), res).what()));
+}
+
+int SqliteCache::exec_retry(SQLite::Database& db, const char* apQueries)
+{
+  return try_exec_retry(db, apQueries, NB_RETRIES);
+}
+
+int try_exec_retry(SQLite::Database& db, SQLite::Statement& st, int nb_retries)
+{
+  int res;
+
+  while (nb_retries--)
+  {
+    res = st.tryExecuteStep();
+    if (SQLITE_DONE == res)  // the statement has finished executing successfully
+      return res;
+  }
+
+  if (SQLITE_ROW == res)
+  {
+    throw std::runtime_error(
+        fmt::format("exec, giving up : {}",
+                    SQLite::Exception("exec() does not expect results. Use executeStep.").what()));
+  }
+  else if (res == sqlite3_errcode(db.getHandle()))
+  {
+    throw std::runtime_error(
+        fmt::format("exec, giving up : {}", SQLite::Exception(db.getHandle(), res).what()));
+  }
+  else
+  {
+    throw std::runtime_error(fmt::format(
+        "exec, giving up : {}", SQLite::Exception("Statement needs to be reseted", res).what()));
+  }
+}
+
+int SqliteCache::exec_retry(SQLite::Database& db, SQLite::Statement& st)
+{
+  try_exec_retry(db, st, NB_RETRIES);
+
+  // Return the number of rows modified by those SQL statements (INSERT, UPDATE or DELETE)
+  return sqlite3_changes(db.getHandle());
+}
+
+bool try_executeStep_retry(SQLite::Database& db, SQLite::Statement& st, int nb_retries)
+{
+  int res;
+  while (nb_retries--)
+  {
+    res = st.tryExecuteStep();
+    if ((SQLITE_ROW != res) && (SQLITE_DONE != res))
+    {
+      std::this_thread::sleep_for(std::chrono::milliseconds(BUSYTIME));
+    }
+    else
+    {
+      return st.hasRow();
+    }
+  }
+  if (res == sqlite3_errcode(db.getHandle()))
+  {
+    throw std::runtime_error(
+        fmt::format("executeStep, giving up : {}", SQLite::Exception(db.getHandle(), res).what()));
+  }
+  else
+  {
+    throw std::runtime_error(
+        fmt::format("executeStep, giving up : {}",
+                    SQLite::Exception("Statement needs to be reseted", res).what()));
+  }
+}
+
+bool SqliteCache::executeStep_retry(SQLite::Database& db, SQLite::Statement& st)
+{
+  return try_executeStep_retry(db, st, NB_RETRIES);
+}
+
+SQLite::Statement SqliteCache::query_retry(const SQLite::Database& aDatabase, const char* apQuery)
+{
+  int nb_retries = NB_RETRIES;
+  while (nb_retries)
+  {
+    try
+    {
+      return Statement(aDatabase, apQuery);
+    }
+    catch (const std::exception& e)
+    {
+      std::this_thread::sleep_for(std::chrono::milliseconds(BUSYTIME));
+      nb_retries--;
+      if (! nb_retries) throw std::runtime_error(fmt::format("query giving up : {}", e.what()));
+    }
+  }
+  throw;
 }
 
 SqliteCache::time_point last_point_of_today()
@@ -134,6 +277,6 @@ SqliteCache::time_point last_point_of_today()
   nowsysstructtm.tm_sec  = 59;
   nowsystmt              = to_<time_t>::_(nowsysstructtm);
   nowsys                 = std::chrono::system_clock::from_time_t(nowsystmt);
-  auto now_midnight          = std__chrono::utc_clock::from_sys(nowsys);
+  auto now_midnight      = std__chrono::utc_clock::from_sys(nowsys);
   return now_midnight;
 }
--- cpp_cached/sqlite.h
+++ cpp_cached/sqlite.h
@@ -22,36 +22,47 @@
 class cpp_cached_API SqliteCache
 {
 
-public:
-	using time_point = std__chrono::utc_clock::time_point;
-	static std::filesystem::path default_root_path() { return std::filesystem::temp_directory_path().append("cache").append("sqlite"); }
-	static constexpr size_t      default_max_size = 2_GB;
+ public:
+  using time_point = std__chrono::utc_clock::time_point;
+  static std::filesystem::path default_root_path()
+  {
+    return std::filesystem::temp_directory_path().append("cache").append("sqlite");
+  }
+  static constexpr size_t default_max_size = 2_GB;
 
-	SqliteCache(const std::filesystem::path& root_path = default_root_path(), uint64_t max_size = default_max_size);
+  SqliteCache(const std::filesystem::path& root_path = default_root_path(),
+              uint64_t                     max_size  = default_max_size);
 
-	SQLite::Database get_db(bool create_tables = false);
+  void create_db();
 
-	bool has(const std::string& key);
+  SQLite::Database get_db(bool can_write);
 
-	void erase(const std::string& key);
+  bool has(const std::string& key);
 
-	template <class T>
-	void set(const std::string& key, const T& value, time_point d = time_point());
+  void erase(const std::string& key);
 
-	template <class T>
-	std::optional<T> get(const std::string& key);
+  template <class T>
+  void set(const std::string& key, const T& value, time_point d = time_point());
 
-	// gets a value, compute it if necessary
-	template <class F>
-	std::invoke_result_t <F> get(const std::string& key, F callback, time_point d = time_point());
+  template <class T>
+  std::optional<T> get(const std::string& key);
 
-private:
-	std::filesystem::path _root_path, _db_name;
-	uint64_t              _max_size;
+  // gets a value, compute it if necessary
+  template <class F>
+  std::invoke_result_t<F> get(const std::string& key, F callback, time_point d = time_point());
 
-	size_t total_size(SQLite::Database& db) const;
-	void   clean_expired(SQLite::Database& db);
-	void   clean_older(SQLite::Database& db, int64_t need_to_free);
+ private:
+  std::filesystem::path _root_path, _db_name;
+  uint64_t              _max_size;
+
+  size_t total_size(SQLite::Database& db) const;
+  void   clean_expired(SQLite::Database& db);
+  void   clean_older(SQLite::Database& db, int64_t need_to_free);
+
+  static int               exec_retry(SQLite::Database& db, const char* apQueries);
+  static int               exec_retry(SQLite::Database& db, SQLite::Statement& st);
+  static bool              executeStep_retry(SQLite::Database& db, SQLite::Statement& st);
+  static SQLite::Statement query_retry(const SQLite::Database& aDatabase, const char* apQuery);
 };
 
 // today 23:59:59
@@ -60,95 +71,131 @@ SqliteCache::time_point last_point_of_today();
 // struct for extracting all fields
 struct cache_row_value
 {
-	std::string key;
-	int64_t     store_time;
-	int64_t     expire_time;
-	int64_t     access_time;
-	int64_t     accesss_count;
-	int64_t     size;
+  std::string key;
+  int64_t     store_time;
+  int64_t     expire_time;
+  int64_t     access_time;
+  int64_t     accesss_count;
+  int64_t     size;
 };
 
 template <class T>
 std::optional<T> SqliteCache::get(const std::string& skey)
 {
-	using namespace SQLite;
-	std::optional<T> res;
-	auto             db = get_db();
-	auto             key = skey.c_str();
-	Statement        query(db, "SELECT * FROM CACHE where key = ?");
-	query.bind(1, key);
-	auto success = query.executeStep();
-	if (!success) return res;
-	auto                               values = query.getColumns<cache_row_value, 6>();
-	using duration = std__chrono::utc_clock::duration;
-	time_point expire_time = time_point(duration(values.expire_time));
-	std__chrono::utc_clock::time_point now = std__chrono::utc_clock::now();
-	// remove row if expired
-	if (now > expire_time)
-	{
-		Statement query2(db, "DELETE FROM cache WHERE key = ?");
-		query2.bind(1, key);
-		query2.exec();
-		return res;
-	}
-	// get blob
-	auto                       col_blob = query.getColumn(6);
-	imemstream                 sin(reinterpret_cast<const char*>(col_blob.getBlob()), col_blob.size());
-	cereal::BinaryInputArchive archive(sin);
-	T                          tmp;
-	archive(tmp);
-	res = tmp;
-	// update counters
-	Statement query2(db, "UPDATE cache SET access_count = access_count + 1 WHERE key=?");
-	query2.bind(1, key);
-	query2.exec();
-	Statement query3(db, "UPDATE cache SET access_time = ? WHERE key=?");
-	duration                           dnow = now.time_since_epoch();
-	bind(query3, dnow.count(), key);
-	query3.exec();
-	return res;
+  using namespace SQLite;
+  std::optional<T> res;
+  std::string      where = "get_db\n";
+  try
+  {
+    auto key = skey.c_str();
+    if (! has(skey)) return res;
+    using duration = std__chrono::utc_clock::duration;
+    time_point expire_time;
+    {
+      auto      db    = get_db(false);
+      Statement query = query_retry(db, "SELECT * FROM CACHE where key = ?");
+      query.bind(1, key);
+      auto success = executeStep_retry(db, query);
+      if (! success) return res;
+      auto values = query.getColumns<cache_row_value, 6>();
+      expire_time = time_point(duration(values.expire_time));
+    }
+    std__chrono::utc_clock::time_point now = std__chrono::utc_clock::now();
+    // remove row if expired
+    if (now > expire_time)
+    {
+      //Statement query2(db, "DELETE FROM cache WHERE key = ?");
+      //query2.bind(1, key);
+      //query2.exec();
+      erase(skey);
+      return res;
+    }
+    {
+      // get blob
+      auto db         = get_db(true);
+      where           = "query";
+      Statement query = query_retry(db, "SELECT * FROM CACHE where key = ?");
+      where           = "bind";
+      query.bind(1, key);
+      where        = "executeStep";
+      auto success = executeStep_retry(db, query);
+      if (! success) return res;
+      where         = "getColumn";
+      auto col_blob = query.getColumn(6);
+      where         = "getblob";
+      imemstream sin(reinterpret_cast<const char*>(col_blob.getBlob()), col_blob.size());
+      cereal::BinaryInputArchive archive(sin);
+      T                          tmp;
+      where = "archive";
+      archive(tmp);
+      res = tmp;
+    }
+    {
+      // update counters
+      auto      db = get_db(true);
+      Statement query2 =
+          query_retry(db, "UPDATE cache SET access_count = access_count + 1 WHERE key=?");
+      query2.bind(1, key);
+      exec_retry(db, query2);
+      //query2.exec();
+      Statement query3 = query_retry(db, "UPDATE cache SET access_time = ? WHERE key=?");
+      duration  dnow   = now.time_since_epoch();
+      bind(query3, dnow.count(), key);
+      exec_retry(db, query3);
+    }
+  }
+  catch (std::exception& e)
+  {
+    fmt::print("failed reading blob : {} \n", where);
+    throw std::runtime_error(fmt::format("failed reading blob {} {}\n", where, e.what()));
+  }
+  return res;
 }
 
 template <class T>
 void SqliteCache::set(const std::string& key, const T& value, time_point d)
 {
-	using namespace SQLite;
-	cache_row_value values{};
-	values.store_time = std__chrono::utc_clock::now().time_since_epoch().count();
-	if (d == time_point()) // standard expiration is today at midnight
-	{
-		d = last_point_of_today();
-	}
-	values.expire_time = d.time_since_epoch().count();
-	values.access_time = values.store_time;
-	auto               db = get_db();
-	Statement          query(db, "INSERT OR REPLACE INTO cache(key,store_time,expire_time,access_time,access_count,size,value) VALUES(?,?,?,?,?,?,?)");
-	std::ostringstream out;
-	cereal::BinaryOutputArchive archive(out);
-	archive(value);
-	size_t  sz = out.str().size();
-	size_t ts = total_size(db);
-	if ((ts + sz) > _max_size) // reclaim expired
-		clean_expired(db);
-	ts = total_size(db);
-	if ((ts + sz) > _max_size)
-	{ // reclaim old
-		int64_t need_to_free = (ts + sz) - _max_size;
-		clean_older(db, need_to_free);
-	}
-    int i_sz = static_cast<int>(sz);
-	bind(query, key.c_str(), values.store_time, values.expire_time, values.access_time, values.accesss_count, i_sz);
-	query.bind(7, reinterpret_cast<const void*>(out.str().c_str()), i_sz);
-	query.exec();
+  using namespace SQLite;
+  cache_row_value values{};
+  values.store_time = std__chrono::utc_clock::now().time_since_epoch().count();
+  if (d == time_point())  // standard expiration is today at midnight
+  {
+    d = last_point_of_today();
+  }
+  values.expire_time             = d.time_since_epoch().count();
+  values.access_time             = values.store_time;
+  auto                        db = get_db(true);
+  Statement                   query(db,
+                                    "INSERT OR REPLACE INTO "
+                                                      "cache(key,store_time,expire_time,access_time,access_count,size,value) "
+                                                      "VALUES(?,?,?,?,?,?,?)");
+  std::ostringstream          out;
+  cereal::BinaryOutputArchive archive(out);
+  archive(value);
+  size_t sz = out.str().size();
+  size_t ts = total_size(db);
+  if ((ts + sz) > _max_size)  // reclaim expired
+    clean_expired(db);
+  ts = total_size(db);
+  if ((ts + sz) > _max_size)
+  {  // reclaim old
+    int64_t need_to_free = (ts + sz) - _max_size;
+    clean_older(db, need_to_free);
+  }
+  int i_sz = static_cast<int>(sz);
+  bind(query, key.c_str(), values.store_time, values.expire_time, values.access_time,
+       values.accesss_count, i_sz);
+  query.bind(7, reinterpret_cast<const void*>(out.str().c_str()), i_sz);
+  query.exec();
 }
 
 template <class F>
-std::invoke_result_t <F> SqliteCache::get(const std::string& key, F callback, time_point d)
+std::invoke_result_t<F> SqliteCache::get(const std::string& key, F callback, time_point d)
 {
-	using T = std::invoke_result_t <F>;
-	std::optional<T> opt = get<T>(key);
-	if (opt) return *opt;
-	T res = callback();
-	set(key, res, d);
-	return res;
+  using T              = std::invoke_result_t<F>;
+  std::optional<T> opt = get<T>(key);
+  if (opt) return *opt;
+  T res = callback();
+  set(key, res, d);
+  return res;
 }
