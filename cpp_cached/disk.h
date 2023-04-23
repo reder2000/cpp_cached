43d0f344f69a338b6369dd8e40c80564312da51d
reder2000 <rouge.richard@free.fr>
2023-04-23 11:49:05 +0200

    cereal cleanup

--- cpp_cached/disk.h
+++ cpp_cached/disk.h
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 // disk cache that writes each cache entry as a file on disk
 // needs long file name support
@@ -10,65 +10,67 @@
 #include <cpp_rutils/require.h>
 
 #include <filesystem>
-#include <cereal/archives/binary.hpp>
 #include <fstream>
 #include <cpp_rutils/secure_deprecate.h>
+#include "serialization.h"
 
+class cpp_cached_API DiskCache
+{
 
-class cpp_cached_API DiskCache {
+ public:
+  DiskCache(
+      std::filesystem::path root_path = std::filesystem::temp_directory_path().append("cache"));
 
-public:
+  template <class T>
+  void push(const std::string& key, const T& value);
 
-	DiskCache(std::filesystem::path root_path =
-		std::filesystem::temp_directory_path().append("cache")) ;
+  [[nodiscard]] bool has(const std::string& key) const;
 
-	template <class T>
-	void push(const std::string& key, const T& value);
+  template <class T>
+  T get(const std::string& key) const;
 
-	[[nodiscard]] bool has(const std::string& key) const;
+ private:
+  std::filesystem::path _root_path;
 
-	template <class T>
-	T get(const std::string& key) const;
-
-private:
-
-	std::filesystem::path _root_path;
-	
-	[[nodiscard]] std::filesystem::path get_full_path_splitted(const std::string& key) const;
-
-
+  [[nodiscard]] std::filesystem::path get_full_path_splitted(const std::string& key) const;
 };
 
-template<class T>
-inline void DiskCache::push(const std::string& key, const T& value)
+template <class T>
+void DiskCache::push(const std::string& key, const T& value)
 {
-	auto fn = get_full_path_splitted(key);
-	auto fp = fn;
-	std::filesystem::create_directories(fp.remove_filename());
-	std::ofstream fout;
-	// Set exceptions to be thrown on failure
-	fout.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	try {
-		fout.open(fn, std::ios::binary);
-		//if ((fout.rdstate() & std::ofstream::failbit) != 0)
-		//	std::cerr << "Error opening " << fn << "\n";
-		cereal::BinaryOutputArchive archive(fout);
-		archive(value);
-	}
-	catch (std::system_error& e) {
-		std::cerr << m_strerror_s(errno) << std::endl;
-		//std::cerr << e.code().message() << std::endl;
-		throw;
-	}
+  auto fn = get_full_path_splitted(key);
+  auto fp = fn;
+  std::filesystem::create_directories(fp.remove_filename());
+  std::ofstream fout;
+  // Set exceptions to be thrown on failure
+  fout.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  try
+  {
+    fout.open(fn, std::ios::binary);
+    //if ((fout.rdstate() & std::ofstream::failbit) != 0)
+    //	std::cerr << "Error opening " << fn << "\n";
+    //cereal::BinaryOutputArchive archive(fout);
+    //    archive(value);
+    fout << cpp_cached_serialization::set_value(value);
+  }
+  catch (std::system_error& e)
+  {
+    std::cerr << m_strerror_s(errno) << std::endl;
+    //std::cerr << e.code().message() << std::endl;
+    throw;
+  }
 }
 
-template<class T>
-inline T DiskCache::get(const std::string& key) const
+template <class T>
+T DiskCache::get(const std::string& key) const
 {
-	auto fp = get_full_path_splitted(key);
-	std::ifstream fout(fp, std::ios::binary);
-	cereal::BinaryInputArchive archive(fout);
-	T res;
-	archive(res);
-	return res;
+  auto              fp = get_full_path_splitted(key);
+  std::ifstream     fin(fp, std::ios::binary);
+  std::stringstream buffer;
+  buffer << fin.rdbuf();
+  T res = cpp_cached_serialization::get_value<T>(buffer.str());
+  //cereal::BinaryInputArchive archive(fout);
+  //T                          res;
+  //archive(res);
+  return res;
 }
--- tests/disk.cpp
+++ tests/disk.cpp
@@ -2,33 +2,36 @@
 
 #define CATCH_CONFIG_ALL_PARTS
 #include <catch2/catch_test_macros.hpp>
+#if defined(PREFERED_SERIALIZATION_cereal)
 #include <cereal/types/vector.hpp>
+#endif
 
-
-TEST_CASE("disk", "[cache][hide]") {
-	DiskCache c;
-	using vi = std::vector<int>;
-	vi v(75);
-	for (size_t i = 0; i < 75; i++)
-		v[i] = i;
-	c.push("1", v);
-	c.push("2", v);
-	std::string big_name;
-	for (size_t i = 0; i < 750; i++)
-		big_name.push_back("abcde01234"[i % 10]);
-	c.push(big_name, v);
-	auto d = c.get<vi>("2");
-	c.get<vi>("1");
-	c.get<vi>(big_name);
+TEST_CASE("disk", "[cache][hide]")
+{
+  DiskCache c;
+  using vi = std::vector<int>;
+  vi v(75);
+  for (size_t i = 0; i < 75; i++)
+    v[i] = i;
+  c.push("1", v);
+  c.push("2", v);
+  std::string big_name;
+  for (size_t i = 0; i < 750; i++)
+    big_name.push_back("abcde01234"[i % 10]);
+  c.push(big_name, v);
+  auto d = c.get<vi>("2");
+  c.get<vi>("1");
+  c.get<vi>(big_name);
 }
 
-TEST_CASE("disk2", "[cache][.hide]") {
-	DiskCache c;
-	using vi = std::vector<int>;
-	vi v(75);
-	c.push("1", v);
-	c.push("2", v);
-	c.push("3", v);
-	auto d = c.get<vi>("3");
-	c.get<vi>("2");
+TEST_CASE("disk2", "[cache][.hide]")
+{
+  DiskCache c;
+  using vi = std::vector<int>;
+  vi v(75);
+  c.push("1", v);
+  c.push("2", v);
+  c.push("3", v);
+  auto d = c.get<vi>("3");
+  c.get<vi>("2");
 }
